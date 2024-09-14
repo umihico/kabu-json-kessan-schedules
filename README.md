17a67fb35dbf80b473ba7465c3144ddef92d690f
umihico <umihico@users.noreply.github.com>
2024-09-14 23:22:05 +0900

    feat: 決算スケジュールを取得して公開

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# kabu-json-kessan-schedules
-【株JSON】全銘柄の決算スケジュールをJSON形式で公開しています
+# 【株JSON】決算スケジュール
+
+全銘柄の決算スケジュールをJSON形式で公開しています。
+
+## 公開データ
+
+- 全銘柄決算スケジュールJSONデータ [https://d1rrtoo3h22gy6.cloudfront.net/kabu-json-kessan-schedules/v1/schedules.json](https://d1rrtoo3h22gy6.cloudfront.net/kabu-json-kessan-schedules/v1/schedules.json)
---
+++ fetch_schedules.py
@@ -0,0 +1,73 @@
+import re
+import requests
+from lxml import html
+import json
+
+# import requests_cache
+# session = requests_cache.CachedSession('cache', expire_after=9999999)
+session = requests.Session()
+
+final_data = []
+for i in range(1, 13):
+    month = f"{i:02d}"
+    for page_index in range(1, 100):
+        try:
+            url = f"""https://www.nikkei.com/markets/kigyo/money-schedule/kessan/?ResultFlag=4&KessanMonth={
+                month}&hm={page_index}"""
+            print(f"month: {month}, page_index: {page_index}")
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            body = response.content.decode("utf-8")
+
+            # "2351～2348件目を表示(全2348件)"
+            REGEX_PATTERN = r"(\d+)～(\d+)件目を表示\(全(\d+)件\)"
+            match = re.search(REGEX_PATTERN, body)
+            first_page_only = match is None  # 1ページしかない場合は、このパターンは見つからない
+            if first_page_only and page_index != 1:
+                raise Exception(
+                    f"Regex pattern not found, but page_index is not 1: {url}")
+            tree = html.fromstring(body)
+            headers = [th.text_content() for th in tree.xpath(
+                "//table[@summary='決算発表スケジュール表']/thead/tr/th")]
+            if headers != ["決算発表日", "証券コード", "会社名", "関連情報", "決算期", "決算種別", "業種", "上場市場"]:
+                raise Exception(
+                    f"Table header is not as expected: {headers}")
+            rows = tree.xpath(
+                "//table[@summary='決算発表スケジュール表']/tbody/tr")
+            if len(rows) == 0:
+                raise Exception(f"rows not found: {url}")
+            for (index, row) in enumerate(rows):
+                cols = [td.text_content() for td in row.xpath(".//td | .//th")]
+                if len(cols) != 8:
+                    raise Exception(
+                        f"Table row does not have 8 columns: {cols}, index: {index}")
+                json_data = {
+                    "決算発表日": cols[0],
+                    "証券コード": cols[1],
+                    "会社名": cols[2],
+                    # "関連情報": cols[3], # 常に'適時開示'の文字列しかないので、無視
+                    "決算期": cols[4],
+                    "決算種別": cols[5],
+                    "業種": cols[6],
+                    "上場市場": cols[7],
+                }
+                final_data.append(json_data)
+                # print(json_data)
+            if first_page_only:
+                break
+            else:
+                first, second, total = match.groups()
+                if int(second) >= int(total):  # 一致するはずだが、一応超えたパターンも考慮
+                    break
+        except Exception as e:
+            print(body)
+            print(url)
+            raise e
+
+print(f"件数: {len(final_data)}")
+
+if len(final_data) < 4000:
+    raise Exception("件数が少なすぎる")
+
+with open("schedules.json", "w", encoding="utf-8") as json_file:
+    json_file.write(json.dumps(final_data, ensure_ascii=False, indent=4))
